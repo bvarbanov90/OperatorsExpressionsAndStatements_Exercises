18ce21850dee567802f4dd7b5f671b0577169bfe
Borislav Varbanov <b.vurbanov@gmail.com>
2013-11-17 16:01:44 +0200

    Corrections for exercise 14

--- ExchangeDynamicBitsOfInt/ExchangeDynamicBitsOfInt.cs
+++ ExchangeDynamicBitsOfInt/ExchangeDynamicBitsOfInt.cs
@@ -16,14 +16,14 @@ class ExchangeDynamicBitsOfInt
 		byte numOfExchangeBits = byte.Parse(Console.ReadLine());
 
 		uint maskFirstPosition = 0;
-		for (int i = firstPosition + numOfExchangeBits - 1; i >= firstPosition; i--)
+		for (int i = firstPosition + numOfExchangeBits; i >= firstPosition; i--)
 		{
 			maskFirstPosition = maskFirstPosition | ((uint)1 << i);
 		}
 		Console.WriteLine("Mask of P: {0:X8}", maskFirstPosition);
 
 		uint maskSecondPosition = 0;
-		for (int i = secondPosition + numOfExchangeBits - 1; i >= secondPosition; i--)
+		for (int i = secondPosition + numOfExchangeBits; i >= secondPosition; i--)
 		{
 			maskSecondPosition = maskSecondPosition | ((uint)1 << i);
 		}
@@ -38,11 +38,13 @@ class ExchangeDynamicBitsOfInt
 		uint resultNumber;
 		if (firstPosition < secondPosition)
 		{
-			resultNumber = (number & resultMask) | (bitsFirstPosition << secondPosition | bitsSecondPosition >> secondPosition);
+			resultNumber = (number & resultMask)
+				| (bitsFirstPosition << (secondPosition - firstPosition) | bitsSecondPosition >> (secondPosition - firstPosition));
 		}
 		else
 		{
-			resultNumber = (number & resultMask) | (bitsFirstPosition >> firstPosition | bitsSecondPosition << firstPosition);
+			resultNumber = (number & resultMask)
+				| (bitsFirstPosition >> (firstPosition - secondPosition) | bitsSecondPosition << (firstPosition - secondPosition));
 		}
 
 		Console.WriteLine("The resulting number after exchanging bits is {0}", resultNumber);
