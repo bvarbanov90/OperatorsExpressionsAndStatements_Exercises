2c9c833dcb477eb4b0780b89408dd14e17cc00ce
Borislav Varbanov <b.vurbanov@gmail.com>
2013-11-17 15:07:49 +0200

    Fix exercise 13

--- ExchangeBitsOfInt/ExchangeBitsOfInt.cs
+++ ExchangeBitsOfInt/ExchangeBitsOfInt.cs
@@ -6,10 +6,10 @@ class ExchangeBitsOfInt
 		Console.Write("Input unsigned integer number: ");
 		uint number = uint.Parse(Console.ReadLine());
 
-		uint bits3_4_5 = number & 0x00000038;
-		uint bits23_24_25 = number & 0x03800000;
-		uint resultsNumber = (number & 0xFC7FFFC7) | (bits3_4_5 << 20 | bits23_24_25 >> 20);
+		uint bits_3_4_5 = number & 0x00000038;
+		uint bits_24_25_26 = number & 0x07000000;
+		uint resultsNumber = (number & 0xF8FFFFC7) | (bits_3_4_5 << 21 | bits_24_25_26 >> 21);
 
-		Console.WriteLine("The resulting number after exchanging bits 3,4,5 with 23,24,25 is {0}", resultsNumber);
+		Console.WriteLine("The resulting number after exchanging bits 3,4,5 with 24,25,26 is {0}", resultsNumber);
 	}
 }
